bb7f8daa5884bb584e774f6434dd275cd46df6b8
leslie tu <chiayin.l.tu@gmail.com>
2023-02-13 17:04:48 -0500

    [SMART] add catch-all page for not-yet-customized state paths

---
+++ src/pages/[state].js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+import DefaultMaintenanceMessage from "../components/DefaultMaintenanceMessage";
+
+// catch all page for unrecognized state / program slug
+const StateCatchAllPage = ({}) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            appName
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <Layout
+      pageTitle={site.siteMetadata.title}
+      variant="default"
+      appName="SMART Child Support"
+    >
+      <Seo
+        title={`${site.siteMetadata.appName} | ${site.siteMetadata.title}`}
+      ></Seo>
+      <div className="mb-4">
+        <DefaultMaintenanceMessage appName={site.siteMetadata.appName} />
+      </div>
+      <p className="t-alignC tf-serif">
+        <strong>Stay tuned &mdash; we&rsquo;ll be back shortly!</strong>
+      </p>
+    </Layout>
+  );
+};
+
+export default StateCatchAllPage;
+
+export const Head = ({ location, params, data, pageContext }) => (
+  <Seo>
+    <title>SMART Child Support | Down for Maintenance</title>
+  </Seo>
+);
